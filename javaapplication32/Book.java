071686a3f14cc4cc48dd2ea7d8aa82cc9316b0a8
DarienCbrCode <142682969+DarienCbrCode@users.noreply.github.com>
2023-11-21 23:53:56 -0500

    Darien

---
+++ javaapplication32/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+    private static int bookNumberCounter = 0;
+    private int bookNumber;
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public Book(String name, Author author, double price, int qty) {
+        this.bookNumber = ++bookNumberCounter;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    
+    @Override
+public String toString() {
+    return String.format("%s by %s (%c) at %s, Price: $%.2f, Qty: %d",
+            name, author.toString(), author.getGender(), author.getEmail(), price, qty);
+}
+}
